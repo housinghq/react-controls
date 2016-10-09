809f77c863df1423d298e7b6c0a709e0e487ff65
Ritesh Kumar <rkritesh078@gmail.com>
2016-10-09 14:00:36 +0530

    chore(packages): updated packages and fixed lint

--- components/App/App.js
+++ components/App/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import styles from './App.scss';
+import React, { Component } from 'react'
+import styles from './App.scss'
 
 export default class App extends Component {
-    render() {
-        return <div className={styles.app} onClick={this.props.onClick}>Hello World</div>;
-    }
+  render () {
+    return <div className={styles.app} onClick={this.props.onClick}>Hello World</div>
+  }
 }
 
+App.propTypes = {
+  onClick: React.PropTypes.func
+}
